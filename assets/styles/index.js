c9292370c8adeb507bca3774db6af5319a6d3815
Tania Novikova <tanianovikova74@gmail.com>
2022-11-24 15:59:14 +0100

    validation fixed

--- assets/styles/index.js
+++ assets/styles/index.js
@@ -1,4 +1,3 @@
-
 let errors = [];
 
 function checkValidity(input){
@@ -9,16 +8,28 @@ function checkValidity(input){
     if (validity.patternMismatch){
         errors.push('Неверный формат заполнения')
     }
+   
 }
 
 function checkAll(testEvent){
+    errors = [];
     testEvent.preventDefault();
-    let inputs = document.querySelectorAll("input");
+    let inputs = document.querySelectorAll('input[type="text"],input[type="tel"],textarea[name="comment"],input[type="file"]');
     for (let input of inputs){
         checkValidity(input);
     }
+    if (document.querySelectorAll('input[name="sex"]:checked').length==0){
+        errors.push( "Пожалуйста, выберите пол питомца" );
+        }
+    if (document.querySelector('select option:checked').value==""){
+        errors.push( "Пожалуйста, укажите породу питомца" );
+        }
+    if (document.querySelectorAll('input[name="food"]:checked').length==0){
+        errors.push( "Пожалуйста, укажите, чем питается ваш питомец" );
+        }
     document.getElementById('errorsInfo').innerHTML=errors.join('.<br>');
 }
+
 
 
 document.querySelector("button").addEventListener('click', checkAll);
